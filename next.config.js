d7cb933387c923e6cd7f17675c5cd6c844c299c0
“Sahithya <sahithya.hanumandla01@gmail.com>
2024-08-19 22:44:58 -0400

    fixing main

---
+++ next.config.js
@@ -0,0 +1,14 @@
+const { hostname } = require('os')
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        remotePatterns:[
+            { protocol:"https",
+            hostname:"images.unsplash.com",}
+           
+        ],
+    },
+};
+
+module.exports = nextConfig
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Intro from "@/components/intro";
+
+
+export default function Home() {
+  return (
+    <main>
+      <Intro />
+    </main>
+  )
+}
